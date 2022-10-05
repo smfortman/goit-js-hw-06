bd8abd0da879e3f4766fdd3aa0df292811a62d1e
Sergei Ageev <sergei.ageev@gmail.com>
2022-10-06 01:41:33 +0300

    added solutions

---
+++ js/task-03.js
@@ -0,0 +1,26 @@
+const images = [
+  {
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+];
+
+const gallary = document.querySelector('ul');
+
+const markup = images
+  .map(
+    ({ url, alt }) =>
+      `<li><img src="${url}" width='300' height='200' alt="${alt}"></li>`,
+  )
+  .join('');
+
+gallary.insertAdjacentHTML('afterbegin', markup);
+gallary.setAttribute('style', 'list-style-type:none');
