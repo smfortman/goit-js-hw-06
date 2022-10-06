655f439f43c8f68bcffec64af204abc54c7e0d49
Sergei Ageev <sergei.ageev@gmail.com>
2022-10-07 00:55:29 +0300

    added solutions

---
+++ js/task-06.js
@@ -0,0 +1,11 @@
+const inputEl = document.querySelector('#validation-input');
+
+inputEl.addEventListener('blur', () => {
+	if (inputEl.value.length === Number(inputEl.dataset.length)) {
+		inputEl.classList.add('valid');
+		inputEl.classList.remove('invalid');
+	} else {
+		inputEl.classList.add('invalid');
+		inputEl.classList.remove('valid');
+	}
+});
