79378ad89abd3b3462cdb461b3969a852f273836
Sergei Ageev <sergei.ageev@gmail.com>
2022-10-07 00:37:57 +0300

    added solutions

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector("body");
+const bgColor = document.querySelector("color");
+const btnChangeColor = document.querySelector(".change-color");
+
+btnChangeColor.addEventListener("click", () => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  bgColor.textContent = getRandomHexColor();
+});
