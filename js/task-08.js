a8f870668c71983ae5791656447e52cd58d4f65a
Sergei Ageev <sergei.ageev@gmail.com>
2022-10-07 00:07:09 +0300

    added solutions

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const formEl = document.querySelector(".login-form");
+formEl.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const {
+    elements: { email, password },
+    } = event.currentTarget;
+    
+    if (email.length === 0 & password.length === 0) {
+        event.currentTarget.reset();
+    } else {
+        alert("Please, fill in all the fields!'");
+    }
+});
+
+
 
