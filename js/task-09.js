d472c2071a588653c9f4aed0a257651e2374fc15
Sergei Ageev <sergei.ageev@gmail.com>
2022-10-09 22:17:53 +0300

    fixed task-06

--- js/task-09.js
+++ js/task-09.js
@@ -8,5 +8,21 @@ const btnChangeColor = document.querySelector(".change-color");
 
 btnChangeColor.addEventListener("click", () => {
   bodyEl.style.backgroundColor = getRandomHexColor();
-  bgColor.textContent = getRandomHexColor();
+  bgColor.textContent = bodyEl.style.backgroundColor;
 });
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+// const bodyEl = document.querySelector('body')
+// const txtColor = document.querySelector('.color')
+// const buttonEl = document.querySelector('.change-color')
+
+// buttonEl.addEventListener('click', changeColor)
+
+// function changeColor(){
+//   bodyEl.style.backgroundColor = getRandomHexColor();
+//   txtColor.textContent = bodyEl.style.backgroundColor
+ 
+// }
